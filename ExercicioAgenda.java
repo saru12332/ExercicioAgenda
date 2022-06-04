3d3358d96406e65b8a9cf4a6936eea4b4925a275
saru12332 <103973651+saru12332@users.noreply.github.com>
2022-06-04 20:35:20 -0300

    Add files via upload

---
+++ ExercicioAgenda.java
@@ -0,0 +1,75 @@
+package JavaProjectAvaliacao;
+
+import java.util.Scanner;
+
+import java.util.Arrays;
+
+public class ExercicioAgenda {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		// Declaracao das variaveis 
+		int i = 0, j = 0, k = 0, linhas = 5, colunas = 5;
+		// Declaracao da variavel de matriz com modulos definidos 
+		String m[][] = new String[linhas][colunas];
+		String x;
+
+		// Estrutura de repeticao numero de linhas da matriz
+		for (i = 0; i < linhas; i++) {
+			// Exibir 'Pessoa' + a contagem de pessoa 
+			System.out.printf("Pessoa %d\n", (i + 1));
+			// Pedir dados ao usuario 
+			System.out.print("Informe seu nome: ");
+			// Ler e guardar dados (string) na matriz 
+			m[i][0] = ler.nextLine();
+			System.out.print("Informe seu endereco: ");
+			m[i][1] = ler.nextLine();
+			System.out.print("Informe seu codigo postal: ");
+			m[i][2] = ler.nextLine();
+			System.out.print("Informe seu bairro: ");
+			m[i][3] = ler.nextLine();
+			System.out.print("Informe seu telefone: ");
+			m[i][4] = ler.nextLine();
+			// Pular linha
+			System.out.printf("\n");
+		}
+		
+		// Estrutura de repeticao numero de linhas da matriz
+		for (i = 0; i < linhas; i++) {
+			// Estrutura de repeticao numero de linhas da matriz 
+			for (j = 0; j < linhas; j++) {
+
+				// Estrutura de decisao para comparar coordenadas da matriz
+				if (m[i][0].compareTo(m[j][0]) < 0) {
+					
+					// Estrutura de repeticao numero de colunas da matriz
+					for (k = 0; k < colunas; k++) {
+						// Estrutura de ordenacao das strings de uma coordenada da matriz 
+						x = m[i][k];
+						m[i][k] = m[j][k];
+						m[j][k] = x;
+
+					}
+				}
+			}
+		}
+		
+		// Estrutura de repeticao numero de linhas da matriz
+		for (i = 0; i < linhas; i++) {
+			// Estrutura de repeticao para repitir uma vez
+			for (j = 0; j < 1; j++) {
+				// Exibir o numero da pessoa + contagem 
+				System.out.printf("Pessoa %d\n", (i + 1));
+				// Exibir os dados inseridos alocados em cada coordenada da matriz, em ordem alfabetica (nome) 
+				System.out.printf("Nome: %s\n", m[i][0]);
+				System.out.printf("Endereco: %s\n", m[i][1]);
+				System.out.printf("Codigo Postal: %s\n", m[i][2]);
+				System.out.printf("Bairro: %s\n", m[i][3]);
+				System.out.printf("Telefone: %s\n", m[i][4]);
+				// Pular linha
+				System.out.printf("\n");
+			}
+		}
+	}
+}
